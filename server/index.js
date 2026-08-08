fc0988a7f21343b3958e5408366e246714d66ccb
Chinmay Betageri <2vd23ci401@klsvdit.edu.in>
2026-08-09 02:35:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const expenseRoutes = require("./routes/expenses");
 const groupRoutes = require("./routes/groups");
 const groupExpenseRoutes = require("./routes/groupExpenses");
 
-
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
@@ -15,25 +14,19 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/groups", groupRoutes);
 app.use("/api/group-expenses", groupExpenseRoutes);
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.use((req, res, next) => {
-  if (req.path.startsWith("/api")) return next();
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
